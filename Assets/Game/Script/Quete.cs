31a0802497779d11045c49467645ae468a2ef2dc
Emery <119424953+EmeryDcs@users.noreply.github.com>
2024-03-02 18:56:47 +0100

    UPD_v0.3
    Retour en arrière, on va définir la quête comme l'objet DontDestroyOnLoad()

--- Assets/Game/Script/Quete.cs
+++ Assets/Game/Script/Quete.cs
@@ -17,9 +17,9 @@ public class Quete : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        idQuete = joueur.GetComponent<TransfertFinQuete>().idQuete;
-        queteEnCours = joueur.GetComponent<TransfertFinQuete>().queteEnCours;
-        choisirSeringue = joueur.GetComponent<TransfertFinQuete>().choixSeringue;
+        idQuete = 1;
+        queteEnCours = false;
+        choisirSeringue = false;
     }
 
     // Update is called once per frame
@@ -37,6 +37,15 @@ public class Quete : MonoBehaviour
                 lancerQuete();
             }
         }
+
+        //Si le joueur clique sur E et qu'il est à côté de la seringue, alors la quête est finie
+        if (joueur.GetComponent<BoxCollider>().bounds.Intersects(seringue.GetComponent<BoxCollider>().bounds)) {
+            if (Input.GetKeyDown(KeyCode.E) && queteEnCours){
+                finirQuete();
+            } else if (Input.GetKeyDown(KeyCode.E) && !queteEnCours){
+                Debug.Log("La quête n'est pas lancée.");
+            }
+        }
     }
 
     private void lancerQuete(){
@@ -47,6 +56,17 @@ public class Quete : MonoBehaviour
                 break;
             case 2:
                 Debug.Log("Quete 2 lancée");
+                break;
+        }
+    }
+
+    private void finirQuete(){
+        switch (idQuete){
+            case 1:
+                Debug.Log("Quete 1 finie");
+                queteEnCours = false;
+                choisirSeringue = true;
+                idQuete = 2;
                 break;
         }
     }
